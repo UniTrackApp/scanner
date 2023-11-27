02e7d243da4ada286f402e563db99d131565b9eb
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-11-27 12:55:39 +0000

    feat: Added __init__ files and changed structure (#14)

---
+++ src/constants/colour.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Colour(Enum):
+    RED = "RED"
+    GREEN = "GREEN"
+    BLUE = "BLUE"
+    AMBER = "AMBER"
---
+++ src/constants/mode.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Mode(Enum):
+    READING = "READING"
+    REGISTER = "REGISTER"
